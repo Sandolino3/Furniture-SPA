ea266b2d2093b18df441e1ef7ef53dce3dd3c506
Sandolino3 <89142522+Sandolino3@users.noreply.github.com>
2025-03-27 18:23:51 +0200

    Add page views structure

---
+++ 01.Furniture/src/app.js
@@ -0,0 +1,28 @@
+// import { page } from "C:/Users/PC/AppData/Local/Microsoft/TypeScript/4.1/node_modules/page/page.mjs";\
+import { page } from "../node_modules/page/page.mjs";
+// Define the routes    
+
+
+page('/', () => {
+    console.log('Home Page');
+});
+page('/details/:id', () => {
+    console.log('Details Page');
+});
+page('/create', () => {
+    console.log('Create Page');
+});
+page('/edit/:id', () => {
+    console.log('Edit Page');
+});
+page('/login', () => {
+    console.log('Login Page');
+});
+page('/register', () => {
+    console.log('Register Page');
+});
+page('/my-furniture', () => {
+    console.log('My Furniture Page');
+});
+// Start the application
+page.start();
--- 01.Furniture/src/lib.js
+++ 01.Furniture/src/lib.js
@@ -1,5 +1,4 @@
 import{html, render} from '../node_modules/lit-html/lit-html.js';
 import {until} from '../node_modules/lit-html/directives/until.js';
-import {page} from '../node_modules/page/page.mjs';
 
-export {html, render, until, page};
+export {html, render, until,};
